a4a5b708cb4df100531bd8a5b071f85e5738382b
Jader Fellipe <jaderfellip21@gmail.com>
2026-01-26 06:44:28 -0400

    chore: inicializacao do projeto

---
+++ src/test/java/com/gerenciadorartistas/backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadorartistas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
